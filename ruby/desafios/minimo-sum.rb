21df4e24c1dbe1c82076d45bb2acebba3bd8ced0
MichelMathos <mmathos@gmail.com>
2023-08-23 23:23:44 -0300

    nova implementação do código

--- ruby/desafios/minimo-sum.rb
+++ ruby/desafios/minimo-sum.rb
@@ -1,34 +1,15 @@
 def min_sum_after_operations(array)
-    #total_sum = 0
-
-    array.sort! #Ordenar o array inicial em ordem crescente
-    
-
-    while array.length > 1
-        #sum = array[0] + array[1]
-        #selecionar os dois primeiros elementos e somar o menor deles ao total
-        #total_sum += sum
-
-        #substituir os dois primeiros elementos pelo menor valor
-        #array[0] = sum
-        #array.shift #remover o segundo elemento
-        
-        #encontra o indice do menor elemento entre o primeiro e o segundo
-        min_index = array[0] < array[1] ? 0 : 1
-        sum = array[min_index] + array[min_index ^1] # ^é o operador XOR
-        total_sum += sum
-
-        #Remove os dois elementos utilizados na soma e insere o resultado
-        array[min_index]
-        array.delete_at(min_index ^1)    
-    end
-
-    #total_sum += array[0] if array.length == 1 #se houver um elemento restantante, adicionálo
-
+    min_val = array.min
+    total_sum = min_val * (array.length -1)
     total_sum
 end
 
-#exemplo de entrada: [4, 3, 5, 10, 11]
-array_A = [3, 6, 2, 8, 7, 5]
-result = min_sum_after_operations(array_A)
-puts "A soma total mínima do array B é: #{result}"
+#example 1: [3,4]
+array_1 = [3,4]
+result_1 = min_sum_after_operations(array_1)
+puts "The total sum os array B[] is: #{result_1}"
+
+#example 2: [2, 4, 1, 3]
+array_2 = [2, 4, 1, 3]
+result_2 = min_sum_after_operations(array_2)
+puts "The total sum of array B[] is: #{result_2}"
