7bb6d0c5040be9100513b88de7de6545e52df5d5
MichelMathos <mmathos@gmail.com>
2023-09-04 23:06:23 -0300

    Foram criados os arquivos method.rb return.rb gem_os.rb

---
+++ ruby/method.rb
@@ -0,0 +1,9 @@
+def talk (first_name, last_name) #definindo uma função chamada 'talk', que recebe dois parâmetros: 'first_name' e 'last_name'
+    puts "Olá, #{first_name} #{last_name}! Como você está?" #esta linha contém o código que será executado quando a função 'talk' for chamada
+end
+
+primeiro = "Michel" 
+segundo = "Matos"
+
+talk(primeiro, segundo) #aqui estamos chamando a função 'talk' que definimos anteriormente. Estamos passando as variaveis 'primeiro' e 'segundo' como
+                        #argumento para a função, ou seja, essas variáveis substituem o modelo definido anteriormente, aplicando seus valores '"Michel"' e '"Matos'".
