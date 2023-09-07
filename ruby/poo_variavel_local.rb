6bea1002ec0f28f0b08f446196ff11f3bef3a8f3
MichelMathos <mmathos@gmail.com>
2023-09-06 23:25:35 -0300

    Criados os arquivos sobre variáveis: local, global, de classe e de instância.

--- ruby/poo_variavel_local.rb
+++ ruby/poo_variavel_local.rb
@@ -6,3 +6,8 @@ def foo
 end
 
 foo
+
+puts "o valor da variável local 1: "
+puts local #é possível que o sistma acuse que a variável 'local' não exista ou não imprima nada.
+#Isso acontece por que a variável 'local' não existe fora da função 'foo'.
+#No entanto, quando 'foo' for chamada será impresso na tela o seu valor.
