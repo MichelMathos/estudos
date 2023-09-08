2ee4fbd1906f28112a21fdb25111b60402359e8f
MichelMathos <mmathos@gmail.com>
2023-09-07 22:26:41 -0300

    criado o arquivo block.rb - ruby avançado I

--- ruby/missoes/poo_projeto_compras/mercado.rb
+++ ruby/missoes/poo_projeto_compras/mercado.rb
@@ -1,6 +1,6 @@
 class Mercado
     def initialize(produto) #O construtor 'initialize' recebe um objeto 'produto' como parâmetro e o armazena em uma instência da classe 'Mercado' para que possa
-        #ser utilizado posteriormente.
+                             #ser utilizado posteriormente.
         @produto = produto #@produto é uma variável de instância que atribui o valor do parâmetro 'produto'
     end
 
