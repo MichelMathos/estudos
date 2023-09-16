3ef4d5bbdbf18f9fb491635ad5c18dec3ea68038
MichelMathos <mmathos@gmail.com>
2023-09-15 23:24:54 -0300

    Atualização do arquivo veiculo.rb

--- ruby/projetos/locadora_veiculos/veiculo.rb
+++ ruby/projetos/locadora_veiculos/veiculo.rb
@@ -1,7 +1,8 @@
 class Vehicle
     attr_reader :brand :model :color :year
 
-    def initialize(brand, model, color, year)
+    def initialize(category, brand, model, color, year)
+        @category = category
         @brand = brand
         @model = model
         @color = color
@@ -10,6 +11,7 @@ class Vehicle
 
     def display
         puts "Dados do veículo:"
+        puts "Categoria: #{category}"
         puts "Marca: #{@brand}"
         puts "Modelo: #{@model}"
         puts "Cor: #{color}"
@@ -18,4 +20,25 @@ class Vehicle
 end
 
 class Car
-    attr_reader :category :door
+    attr_reader :doors
+
+    def initialize(category, brand, model, color, year, doors)
+        super(category, brand, model, color, year)
+        @door = doors
+    end
+
+    def display
+        super
+        puts "Portas: #{doors}"
+    end
+end
+
+class motorcycle
+    def initialize(category, brand, model, color, year)
+        super(category, brand, model, color, year)
+    end
+     def display
+        super
+     end
+    end
+    
