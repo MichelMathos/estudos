33e0e37ef68873f709862be7714bab8172d09332
MichelMathos <mmathos@gmail.com>
2023-09-22 22:44:28 -0300

    conexão com o banco de dados

--- ruby/projetos/locadora_veiculos/nada.rb
+++ ruby/projetos/locadora_veiculos/nada.rb
@@ -1,1 +1,21 @@
-gdfghdf
+# Acessando a classe mysql2 (gem mysql2)
+require 'mysql2'
+
+# Fazendo a conexão com o banco de dados onde db é a variável
+# responsável por fazer a conexão
+db = Mysql2::Client.new(:host => "localhost",
+                        :username => "unifeg",
+                        :password => "unifeg123",
+                        :database => "unifeg_dsv")
+
+# Utilizando a variável rs como um vetor que buscará os registros com 
+# o método .query, seguido do comando SQL
+rs = db.query("select * from client")
+
+# Utilizamos o método .each para carregar o vetor rs com os registros,
+# no qual row é a linha da tabela client.
+rs.each do |row|
+    puts row
+end
+
+puts ".. | fimn da aplicação"
