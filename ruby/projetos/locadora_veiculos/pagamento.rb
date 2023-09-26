3071a4ce5a86e69b3e6a67861d44f052d0e60cfb
MichelMathos <mmathos@gmail.com>
2023-09-26 06:57:30 -0300

    Atualização de códigos

--- ruby/projetos/locadora_veiculos/pagamento.rb
+++ ruby/projetos/locadora_veiculos/pagamento.rb
@@ -1,40 +1,41 @@
 class Pagamento
-    attr_reader :valor
-  
-    def initialize(valor)
-      @valor = valor
-    end
+  attr_reader :valor
+
+  def initialize(valor)
+    @valor = valor
   end
-  
-  class PagamentoEmDinheiro < Pagamento
-    def exibir
-      puts "Pagamento em dinheiro - valor da diária: R$#{@valor}"
-    end
+end
+
+class PagamentoEmDinheiro < Pagamento
+  def exibir
+    puts "Pagamento em dinheiro - valor da diária R$#{ @valor }"
   end
-  
-  class PagamentoComCartaoDeDebito < Pagamento
-    def exibir
-      puts "Pagamento com cartão de débito - Valor da diária: R$#{@valor}"
-    end
+end
+
+class PagamentoComCartaoDeDebito < Pagamento
+  def exibir
+    puts "Pagamento com cartão de débito - valor da diária: R$#{ @valor }"
   end
-  class PagamentoComCartaoDeCredito < Pagamento
-    attr_reader :parcelas
-  
-    def initialize(valor, parcelas)
-      super(valor)
-      @parcelas = parcelas
-    end
-  
-    def calcula_juros(dias_de_locacao)
-      if parcelas > 1
-        juros = valor * 0.02 * dias_de_locacao # Taxa de juros mensal de 2%
-        return juros * parcelas
-      end
-      0
-    end 
-  
-    def exibir
-      puts "Pagamento com cartão de crédito - Valor da diária: R$#{@valor} (#{@parcelas}x)"
-    end
+end
+
+class PagamentoComCartaoDeCredito < Pagamento
+  attr_reader :parcelas
+
+  def initialize(valor, parcelas)
+    super(valor)
+    @parcelas = parcelas
   end
-  
+#end
+
+def calcula_juros(dias_de_locacao)
+  if parcelas > 1
+    juros = valor * 0.2 * dias_de_locacao # Definida taxa de juros mensal de 2%
+    return juros * parcelas
+  end
+  0
+end
+
+  def exibir 
+    puts "Pagamento com cartão de crédito - Valor diária: R$#{ @valor } (#{@parcelas}X)"
+  end
+end
