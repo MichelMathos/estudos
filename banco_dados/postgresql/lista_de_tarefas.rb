bebdf9f3a7966e26540c7681730f58e0ee866f25
MichelMathos <mmathos@gmail.com>
2023-09-28 23:41:49 -0300

    refactor(lista_de_tarefas.rb): realiza refatoração no código

--- banco_dados/postgresql/lista_de_tarefas.rb
+++ banco_dados/postgresql/lista_de_tarefas.rb
@@ -16,6 +16,23 @@ def get_all_task(conn)
     conn.exec('SELECT * FROM tasks;')
 end
 
-
-
 # Conecte-se ao banco de dados
+conn = PG.connect(dbname: 'create_task_table', user: 'postgres', password: 'Trainee1@', host: 'localhost')
+
+# Crie a tabela de tarefas se não existir
+
+create_tasks_table(conn)
+
+# Adicione algumas tarefas
+add_task(conn, 'Fazer compras')
+add_task(conn, 'Estudar Ruby')
+add_task(conn, 'Praticar PostgreSQL')
+
+# Obtenha e imprima todas as tarefas
+result = get_all_tasks(conn)
+result.each do |row|
+    puts "ID: #{row['id']} Descrição: #{row['description']}"
+end
+
+# Feche a conexão
+conn.close
