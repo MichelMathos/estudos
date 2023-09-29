caa7d4eddc0b243f97faf788102302fd533d7658
MichelMathos <mmathos@gmail.com>
2023-09-28 22:53:35 -0300

    feat(lista_de_tarefas): adiciona arquivo lista_de_tarefas.rb

---
+++ banco_dados/postgresql/lista_de_tarefas.rb
@@ -0,0 +1,19 @@
+require 'pg'
+
+# Método para criar a tabela de tarefas
+
+def create_tasks_table(conn)
+    conn.exec('CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY, description TEXT);')
+end
+
+# Método para adicionar uma tarefa
+def add_task(conn, descripton)
+    conn.exec_params('INSERT INTO tasks (description) values ($1);', [description])
+end
+
+# Método para obter todas as tarefas
+def get_all_task(conn)
+    conn.exec('SELECT * FROM tasks;')
+end
+
+# Conecte-se ao banco de dados
