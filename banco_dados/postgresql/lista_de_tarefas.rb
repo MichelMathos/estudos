81a9cc043e88002a8efc7a8e67abe9ca387ceb21
MichelMathos <mmathos@gmail.com>
2023-09-28 23:56:56 -0300

    fix(lista_de_tarefas): correção de erros linha 6 e linha 24

--- banco_dados/postgresql/lista_de_tarefas.rb
+++ banco_dados/postgresql/lista_de_tarefas.rb
@@ -1,18 +1,17 @@
 require 'pg'
 
 # Método para criar a tabela de tarefas
-
 def create_tasks_table(conn)
-    conn.exec('CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY, description TEXT);')
+    conn.exec('CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, description TEXT);')
 end
 
 # Método para adicionar uma tarefa
-def add_task(conn, descripton)
-    conn.exec_params('INSERT INTO tasks (description) values ($1);', [description])
+def add_task(conn, description)
+    conn.exec_params('INSERT INTO tasks (description) VALUES ($1);', [description])
 end
 
 # Método para obter todas as tarefas
-def get_all_task(conn)
+def get_all_tasks(conn)
     conn.exec('SELECT * FROM tasks;')
 end
 
@@ -20,7 +19,6 @@ end
 conn = PG.connect(dbname: 'create_task_table', user: 'postgres', password: 'Trainee1@', host: 'localhost')
 
 # Crie a tabela de tarefas se não existir
-
 create_tasks_table(conn)
 
 # Adicione algumas tarefas
@@ -29,8 +27,8 @@ add_task(conn, 'Estudar Ruby')
 add_task(conn, 'Praticar PostgreSQL')
 
 # Obtenha e imprima todas as tarefas
-result = get_all_tasks(conn)
-result.each do |row|
+results = get_all_tasks(conn)
+results.each do |row|
     puts "ID: #{row['id']} Descrição: #{row['description']}"
 end
 
