81a7f638ff990aa34085b8de381ad94762a7ad97
MichelMathos <mmathos@gmail.com>
2023-10-16 23:57:21 -0300

    feat(controle_estoque): criada classe TelaBoasVindas

---
+++ banco_dados/postgresql/controle_estoque/tela_boas_vindas.rb
@@ -0,0 +1,8 @@
+class TelaBoasVindas
+    attr_accessor :sistema_controle_estoque
+
+    def initialize(sistema_controle_estoque)
+        @sistema_controle_estoque = sistema_controle_estoque
+    end
+
+end
