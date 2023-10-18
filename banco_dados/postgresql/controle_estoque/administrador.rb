3824ffcb19c8a7eedca03d92f611d6710651200c
MichelMathos <mmathos@gmail.com>
2023-10-17 22:48:39 -0300

    feat(controle_estoque): métodos adicionar usuario, listar usuarios, adicionar produto

--- banco_dados/postgresql/controle_estoque/administrador.rb
+++ banco_dados/postgresql/controle_estoque/administrador.rb
@@ -5,4 +5,26 @@ class Administrador < Usuario
         super(id_usuario, codigo, senha, nome, cpf, endereco, telefone, email)
     end
 
+    # Método específico para administrador: Adicionar um novo usuário
+    def adicionar_usuario(codigo, senha, nome, cpf, endereco, telefone, email)
+        novo_usuario = Usuario.new(codigo, senha, nome, cpf, endereco, telefone, email)
+        SistemaControleEstoque.instance.adicinar_usuario(novo_usuario)
+    end
+
+    # Método específico para admnistrador: Listar todos os usuários
+    def listar_usuarios
+        usuarios.each do |usuario|
+            puts "#{usuario.id_usuario} - #{usuario.nome} (#{usuario.class})"
+        end
+    end
+
+    # Método específico para administrador: Adicionar novo produto
+    def adicionar_produto(nome, descricao, prec_unitario, fornecedor)
+        novo_produto = Produto.new(nome, descricao, preco_unitario, fornecedor)
+        SistemaControleEstoque.instance.adicinar_protudo(novo_produto)
+    end
+
+    # Método específico para administrador: Listar todos os produtos
+    
+
 end
