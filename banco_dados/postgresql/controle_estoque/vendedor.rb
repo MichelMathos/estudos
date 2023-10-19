0620c633462ab2ef6c44a178fc93089c7394a27d
MichelMathos <mmathos@gmail.com>
2023-10-18 23:24:36 -0300

    feat(controle_estoque): Classe vendedor, método para realizar venda e atualizar quantidade em estoque

--- banco_dados/postgresql/controle_estoque/vendedor.rb
+++ banco_dados/postgresql/controle_estoque/vendedor.rb
@@ -3,7 +3,30 @@ require_relative 'usuario'
 class Vendedor < Usuario
     def initialize(id_usuario, codigo, senha, nome, cpf, endereco, telefone, email)
         super(id_usuario, codigo, senha, nome, cpf, endereco, email)
+        @vendas =[] # Lista para armazenar as vendas realizads pelo vendedor
     end
+
+    # Método específico para vendedor: Realizar venda
+    def realizar_venda(id_produto, quantidade)
+        produto = SistemaControleEstoque.instance.encontrar_produto_por_id(id_produto)
+
+        if produto && produto.quantidade_em_estoque >= quantidade
+            venda = { produto: produto, quantidade: quantidade, data: Time.now}
+            @venda << venda
+
+            # Atualiza a quantidade em estoque do produto
+            produto.quantidade_em_estoque -= quantidade
+
+            puts "Venda realizada com sucesso!"
+
+        elsif produto.nil?
+            puts "Protudo não encontrado."
+        else
+            puts "Quantidade em estoque insuficiente."
+        end
+    end
+    
+
 
 end
 
