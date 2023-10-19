ac18e417cf487bc898d171379509be8db802fb29
MichelMathos <mmathos@gmail.com>
2023-10-18 23:53:26 -0300

    feat(estoquista): Método para adicionar produto ao estoque

--- banco_dados/postgresql/controle_estoque/estoquista.rb
+++ banco_dados/postgresql/controle_estoque/estoquista.rb
@@ -1,7 +1,24 @@
 require_relative 'usuario'
+
 class Estoquista < Usuario
     def initialize(id, usuario, codigo, senha, nome, cpf, endereco, telefone, email)
         super(id_usuario, codigo, senha, nome, cpf, endereco, telefone, email)
     end
+
+    # Método específico para estoquista: Adicionar produto ao estoque
+    def adicionar_produto_ao_estoque(id_protudo, quantidade)
+        produto = SistemaControleEstoque.instance.encontrar_produto_por_id(id_produto)
+
+        if produto
+            produto.quantidade_em_estoque += quantidade
+            puts "#{quantidade} unidades do produto #{produto.nome} adicionada ao estoque."
+        else
+            puts "Produto não encontrado."
+        end
+    end
+    
+
+
+
 
 end
