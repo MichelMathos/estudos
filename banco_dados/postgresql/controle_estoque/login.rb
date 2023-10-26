da5bd8dc8375921fcb2e2248b46d3bad58d0c1a0
MichelMathos <mmathos@gmail.com>
2023-10-25 23:19:54 -0300

    feat(login): Definição das funções que verificam as credenciais de usuários e administrador

--- banco_dados/postgresql/controle_estoque/login.rb
+++ banco_dados/postgresql/controle_estoque/login.rb
@@ -32,14 +32,14 @@ class ControleEstoque
     private
 
     def admin_login
-        # Lógica de login paar o administrador (verifica código e senha)
+        # Lógica de login para o administrador
         puts "Digite o código de administrador: "
         codigo = gets.chomp
         puts "Digite a senha do administrador: "
         senha = gets.chomp
 
-        # Verifica as credenciai
-        if cverificar_credenciais_admin(codigo, senha)
+        # Verifica as credenciais do administrador
+        if verificar_credenciais_admin(codigo, senha)
             administrador = Administrador.new("admin_id", codigo, senha, "Administrador", "123456789", "Endereço", "123-456-7890", "admin@admin.com")
             administrador.exibir_dashboard
         else
@@ -54,12 +54,25 @@ class ControleEstoque
         puts "Digite sua senha de vendedor: "
         senha = gets.chomp
 
-        # Verifica as credenciais (simplificado)
+        # Verifica as credenciais do vendedor
         if verificar_credenciais_vendedor(codigo, senha)
-            vendedor = Vendedor.new("vendedor_id", codifo, senha, "Vendedor", "987654321", "Endereco", "987-654-3210", "vendedor@vendedor.com")
+            vendedor = Vendedor.new("vendedor_id", codigo, senha, "Vendedor", "987654321", "Endereco", "987-654-3210", "vendedor@vendedor.com")
             vendedor.exibir_dashboard
         else
             puts "Credenciais de vendedor inválidas."
         end
     end
+
+    # Funções que verificam as credenciais de usuário e administrador
+    def verificar_credenciais_admin(codigo, senha)
+        result = @conn.exec_params("SELECT * FROM usuarios WHERE codigo = $1 AND senha = $2 AND papel = 'admin'", [codigo, senha])
+        return !resulkt.num_tuples.zero?
+    end
+
+    def verificar_credenciais_vendedor(codigo, senha)
+        result = @conn.exec_params("SELECT * FROM usuarios where codigo = $1 AND senha = $2 AND papel = 'vendedor'", [codigo, senha])
+        return !result.num_tuples.zero?
+    end
+end
+
     
