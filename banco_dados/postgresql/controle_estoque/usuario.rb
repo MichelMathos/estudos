77de6f80d982e4aa25acfcae3783d3d6ad51a434
MichelMathos <mmathos@gmail.com>
2023-10-26 22:58:38 -0300

    feat(usuario): Conexão ao banco de dados

--- banco_dados/postgresql/controle_estoque/usuario.rb
+++ banco_dados/postgresql/controle_estoque/usuario.rb
@@ -11,4 +11,27 @@ class Usuario
         @telefone = telefone
         @email = email
     end
+    
+    def self.encontrar_por_codigo_e_senha(codigo, senha)
+        conn = PG.connect(dbname: 'nome_do_banco', user: 'usuario', password: 'senha')
+        result = conn.exec_params("SELECT * FROM usuarios WHERE codigo = $1 AND senha = $2", [codigo, senha])
+        conn.close 
+
+        if result.num_tuples.zero?
+            nil?
+        else
+            usuario_data = result[0]
+            Usuario.new(
+                usuario_data['id'].to_i,
+                usuario_data['codigo'],
+                usuario_data['senha'],
+                usuario_data['nome'],
+                usuario_data['cpf'],
+                usuario_data['endereco'],
+                usuario_data['telefone'],
+                usuario_data['email']
+            )
+        end
+    end
 end
+
