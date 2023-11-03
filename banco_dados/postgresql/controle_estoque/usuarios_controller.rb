f800f20f17a69b82ca4f71c58d21ab22ba130d5d
MichelMathos <mmathos@gmail.com>
2023-11-02 23:18:30 -0300

    feat(usuarios_controller): Método para criar usuários

--- banco_dados/postgresql/controle_estoque/usuarios_controller.rb
+++ banco_dados/postgresql/controle_estoque/usuarios_controller.rb
@@ -2,3 +2,12 @@ require 'pg'
 
 # Conexão com o banco de dados
 conn = PG.connect(dbname: 'controle_estoque', user: 'postgresql', password: 'Trainee1@')
+
+# Criando a tabela "usuarios" no banco de dados
+conn.exec(<<-SQL)
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id SERIAL PROMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL
+    )
+SQL
