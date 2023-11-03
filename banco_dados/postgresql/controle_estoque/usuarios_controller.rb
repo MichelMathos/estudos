3e2b69fce33410f7ed7d52b0dd58b095a1bfce1d
MichelMathos <mmathos@gmail.com>
2023-11-02 23:25:55 -0300

    feat(usuarios_controller): Implementada o método para listar os usuários

--- banco_dados/postgresql/controle_estoque/usuarios_controller.rb
+++ banco_dados/postgresql/controle_estoque/usuarios_controller.rb
@@ -11,3 +11,19 @@ conn.exec(<<-SQL)
         email VARCHAR(100) NOT NULL
     )
 SQL
+
+class UsuariosController
+    def initialize(conn)
+        @conn = conn
+    end
+
+    def criar_usuario(nome, email)
+        @con.exec_params("INSERT INTO usuario (nome, email) VALUES ($1, $2)", [nome, email])
+    end
+
+    def listar_usuarios
+        result = @conn.exec_params("SELECT * FROM usuarios")
+        result.map { |row| { id: row['id'].to_i, nome: row['nome'], email: row['email']}}
+    end
+
+    
