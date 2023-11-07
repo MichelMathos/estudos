de8b4090287eb0297afc45f8ffee885a7e5f284f
MichelMathos <mmathos@gmail.com>
2023-11-07 20:40:07 -0300

    fix(administrador): Correção no construtor, id_usuario para id em super

--- banco_dados/postgresql/controle_estoque/administrador.rb
+++ banco_dados/postgresql/controle_estoque/administrador.rb
@@ -1,6 +1,6 @@
 class Administrador < Usuario
     def initialize(id_usuario, codigo, senha, nome, cpf, endereco, telefone, email)
-        super(id_usuario, codigo, senha, nome, cpf, endereco, telefone, email)
+        super(id, codigo, senha, nome, cpf, endereco, telefone, email)
     end
 
     # Método específico para administrador: Adicionar um novo usuário
