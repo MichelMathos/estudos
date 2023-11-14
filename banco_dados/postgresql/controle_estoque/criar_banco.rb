7f9afa9124d0fbb8bc487e572bd2030223a77dc8
MichelMathos <mmathos@gmail.com>
2023-11-13 21:54:02 -0300

    feat(criar_banco): Cria a tabela usuarios.

--- banco_dados/postgresql/controle_estoque/criar_banco.rb
+++ banco_dados/postgresql/controle_estoque/criar_banco.rb
@@ -11,3 +11,18 @@ def criar_banco_de_dados(dbname, user, password, host, port)
         conn&.close
     end
 end
+
+def criar_tabelas(conn)
+    begin
+        conn.exec('
+            CREATE TABLE IF NOT EXISTS usuarios (
+                id SERIAL PRIMARY KEY,
+                codigo VARCHAR(50) NOT NULL,
+                senha VARCHAR(100) NOT NULL,
+                nome VARCHAR(100) NOT NULL,
+                cpf VARCHAR(14) NOT NULL,
+                endereço TEXT,
+                telefone VARCHAR(15),
+                email VARCHAR(20) NOT NULL
+
+            )')
