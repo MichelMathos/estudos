6492ece2960b232a1c070977b32057ea6fa6df0a
MichelMathos <mmathos@gmail.com>
2023-11-15 13:42:06 -0300

    refactor(login): atualização do código que faz a conexão com o banco de dados.

--- banco_dados/postgresql/controle_estoque/login.rb
+++ banco_dados/postgresql/controle_estoque/login.rb
@@ -1,33 +1,15 @@
-require 'pg'
+require 'conexão_banco'
 require_relative 'sistema_controle_estoque'
-
-# Configurações do banco de dados
-DB_CONFIG ={
-    dbname: 'controle_estoque',
-    user: 'postgres',
-    password: 'Trainee1@',
-    host: 'localhost',
-    port: 5432
-}.freeze
-
-conn = nil
-# Tenta conectar ao banco de dados
-begin
-    conn = PG.connect(DB_CONFIG)
-    puts "Conectado ao banco de dados #{DB_CONFIG[:dbname]}."
-rescue PG::ConnectionBad
-    puts "Banco de dados não encontrado. Criando o banco de dados..."
-    conn = PG.connect(dbname: 'postgres', user: user, password: password, host: host, port: port)
-    conn.close
-    criar_banco_de_dados(conn, DB_CONFIG[:dbname])
-    conn = PG.connect(BD_CONFIG)
-end
-
-
 
 class ControleEstoque
     def initialize
         @sistema = SistemaControleEstoque.new
+        @conn = ConexaoBanco.conectar 
+        puts "Conectado ao Banco de dados #{ConexaoBanco::DB_CONFIG[:dbname]}"
+    rescue PG::ConnectionBad
+        puts "Banco de dados não encontrado. Criando banco de dados..."
+        criar_banco_de_dados
+        retry
     end
 
     def iniciar
@@ -76,37 +58,31 @@ class ControleEstoque
         codigo = gets.chomp
         puts "Digite sua senha de vendedor: "
         senha = gets.chomp
-
-        # Verifica as credenciais do vendedor
-        if verificar_credenciais_vendedor(codigo, senha)
-            vendedor = Vendedor.new("vendedor_id", codigo, senha, "Vendedor", "987654321", "Endereco", "987-654-3210", "vendedor@vendedor.com")
-            vendedor.exibir_dashboard
-        else
-            puts "Credenciais de vendedor inválidas."
-        end
     end
 
-    # Funções que verificam as credenciais de usuário e administrador
     def verificar_credenciais_admin(codigo, senha)
         result = @conn.exec_params("SELECT * FROM usuarios WHERE codigo = $1 AND senha = $2 AND papel = 'admin'", [codigo, senha])
-        return !result.num_tuples.zero?
-    end
-
-    def verificar_credenciais_vendedor(codigo, senha)
+        !result.num_tuples.zero?
+      end
+    
+      def verificar_credenciais_vendedor(codigo, senha)
         result = @conn.exec_params("SELECT * FROM usuarios WHERE codigo = $1 AND senha = $2 AND papel = 'vendedor'", [codigo, senha])
-        return !result.num_tuples.zero?
+        !result.num_tuples.zero?
+      end
+    
+      def criar_banco_de_dados
+        begin
+          conn = PG.connect(dbname: 'postgres', user: ConexaoBanco::DB_CONFIG[:user], password: ConexaoBanco::DB_CONFIG[:password], host: ConexaoBanco::DB_CONFIG[:host], port: ConexaoBanco::DB_CONFIG[:port])
+          conn.exec("CREATE DATABASE #{ConexaoBanco::DB_CONFIG[:dbname]}")
+          puts "Banco de dados #{ConexaoBanco::DB_CONFIG[:dbname]} criado com sucesso."
+        rescue PG::Error => e
+          puts "Erro ao criar o banco de dados #{ConexaoBanco::DB_CONFIG[:dbname]}: #{e.message}"
+        ensure
+          conn&.close
+        end
+      end
     end
-end
-
-def criar_banco_de_dados(conn,dbname)
-    begin
-        conn.exec("CREATE DATABASE #{dbname}")
-        puts "Banco de dados #{dbname} criado com sucesso."
-    rescue PG::ERROR => e
-        puts "Erro ao criar o banco de dados #{dbname}: #{e.message}"
-    end
-end
-
-# Inicializar o controle de estoque
-controle_estoque = ControleEstoque.new
-controle_estoque.iniciar
+    
+    # Inicializar o controle de estoque
+    controle_estoque = ControleEstoque.new
+    controle_estoque.iniciar
