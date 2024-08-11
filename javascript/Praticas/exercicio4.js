fa010544a8eb98d1d23c92019b07711850feaf63
Michel Matos <mmathos@gmail.com>
2024-08-10 22:35:52 -0300

    função que escreva o nome

---
+++ javascript/Praticas/exercicio4.js
@@ -0,0 +1,14 @@
+/*
+Crie uma função que escfeva seu nome
+*/
+
+function meuNome(){
+    console.log('Michel Matos 1');
+}
+ meuNome();
+
+ // invocada imediatamente
+
+ (function (){
+    console.log('Michel Matos 2');
+})();
