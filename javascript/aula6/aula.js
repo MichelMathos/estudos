86b373fa4671be2880b5e801911b8d40305954f6
Michel Matos <mmathos@gmail.com>
2024-08-26 23:52:06 -0300

    Inserindo, substituindo e removendo itens do array

--- javascript/aula6/aula.js
+++ javascript/aula6/aula.js
@@ -3,3 +3,23 @@
 const alunos = ['João', 'Victor', 'Marina'];
 console.log(alunos);
 console.log(alunos[0]);
+
+// add itens à lista
+
+alunos.push('Renan'); // push insere dinamicamente no final do array
+console.log(alunos);
+
+alunos[4] = 'Vinícius'; //inserindo diretamente na posição
+console.log(alunos);
+
+// substituindo um item da lista
+
+alunos[3] = 'Jorge';
+console.log(alunos);
+
+// removendo itens
+alunos.pop(); // remove o último item
+console.log(alunos);
+
+console.log(alunos.pop()); // imprime o ultimo item antes de remover
+console.log(alunos);
