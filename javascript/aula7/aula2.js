1224e91f8dd89eb377d94b5eb0e5d5721e2553e9
Michel Matos <mmathos@gmail.com>
2024-08-27 23:18:49 -0300

    atualização

--- javascript/aula7/aula2.js
+++ javascript/aula7/aula2.js
@@ -11,6 +11,6 @@ let soma = 0;
 for (let i = 0; i < notas.length; i++) {
    const nota = notas[i];
    soma = soma + nota;
-     }
+}   
 const media = soma / notas.length;
 console.log(media.toFixed(2));
