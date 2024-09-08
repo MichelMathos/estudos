dc704d675aecf6f6b6c3a6149dd254aa19c20294
Michel Matos <mmathos@gmail.com>
2024-09-08 00:47:17 -0300

    enunciado

---
+++ javascript/Praticas/exercicio11.js
@@ -0,0 +1,17 @@
+/*
+    Uma sala contem 5 alunos e para cada aluno foi oferteado um número de 1 - 100.
+    Faça um programa que receba os 5 números sorteados para os alunos e mostre o
+    maior número sorteado.
+
+    Dados de entrada:
+
+    5
+    50
+    10
+    98
+    23
+
+    Saída:
+    98
+*/
+
