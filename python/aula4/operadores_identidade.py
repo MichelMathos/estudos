cf5b7791d5e37a6a2afeeded8f7a95ed14574efd
Michel Matos <mmathos@gmail.com>
2024-09-20 17:21:44 -0300

    operadores de identidade

---
+++ python/aula4/operadores_identidade.py
@@ -0,0 +1,7 @@
+# operadores de identidade são utilizados para comparar se os objetos ocvupam a mesma região de memória
+
+saldo = 1000
+limite = 500
+
+print(saldo is limite)
+print( saldo is not limite)
