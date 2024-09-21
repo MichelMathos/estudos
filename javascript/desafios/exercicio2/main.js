807fa4b58c7592c37430dde769bb10a7f7139df4
Michel Matos <mmathos@gmail.com>
2024-09-20 22:24:42 -0300

    desenvolvimento do desafio maior numero par e menor numero impar

--- javascript/desafios/exercicio2/main.js
+++ javascript/desafios/exercicio2/main.js
@@ -1,16 +1,39 @@
 /*
-    2) Faça um programa que receba N (quantidades de números) e seus respectivos valores.
-    Imprima o maior número par e o menor número ímpar. 
+    2) Faça um programa que receba uma sequência de números e imprima o maior número par e o menor número ímpar.
 
-            Exemplo:
-                Entradas:
-                    5
-                    3
-                    4
-                    1
-                    10
-                    8
-                Saída:
-                    Maior número par: 10
-                    Menor número ímpar: 1
+    Exemplo:
+        Entradas:
+            5
+            13
+            4
+            7
+            10
+            8
+        Saída:
+            Maior número par: 10
+            Menor número ímpar: 5
 */
+
+const { gets, print } = require('./funcoes-auxiliares');
+
+const n = gets(); // Convertendo a quantidade de números para inteiro
+let maiorNumeroPar = null;
+let menorNumeroImpar = null;
+
+for (let i = 0; i < n; i++) {
+    const numero = gets(); // Convertendo a entrada para número inteiro
+
+    if (numero % 2 === 0) { // Verifica se o número é par
+        if (maiorNumeroPar === null || numero > maiorNumeroPar) {
+            maiorNumeroPar = numero;
+        }
+    } else { // Caso contrário, é ímpar
+        if (menorNumeroImpar === null || numero < menorNumeroImpar) {
+            menorNumeroImpar = numero;
+        }
+    }
+}
+
+// Exibe os resultados
+    print("Maior número par: " + maiorNumeroPar);
+    print("Menor número ímpar: " + menorNumeroImpar);
